bf09cd52b290f7452ecb9252ba2dff2d46d20b12
darkmagician <flyincloud@gmail.com>
2016-04-23 17:09:03 +0800

    fix start

--- ssh/onstart.sh
+++ ssh/onstart.sh
@@ -1,9 +1,10 @@
+export >>/vars.txt
 if [ -n "$MAILGUN_API" ] && [ -n "$MAILGUN_DOMAIN" ] && [ -n "$MAILGUN_TO" ]
 then
-curl -s --user 'api:$MAILGUN_API' \
+curl -s --user "api:$MAILGUN_API" \
     https://api.mailgun.net/v3/$MAILGUN_DOMAIN/messages \
-    -F from='Mailgun Sandbox <postmaster@$MAILGUN_DOMAIN>' \
+    -F from="Mailgun Sandbox <postmaster@$MAILGUN_DOMAIN>" \
     -F to=$MAILGUN_TO \
-    -F subject='service is started!' \
-    -F text='started!'	
+    -F subject="service is started!" \
+    -F text="started!"	
 fi
