3746c0dc4c59ea776721c61ff2873c9a87c3ab8e
darkmagician <flyincloud@gmail.com>
2016-04-23 20:03:49 +0800

    add $__DEFAULT_DOMAIN_NAME__

--- ssh/onstart.sh
+++ ssh/onstart.sh
@@ -5,6 +5,6 @@ curl -s --user "api:$MAILGUN_API" \
     https://api.mailgun.net/v3/$MAILGUN_DOMAIN/messages \
     -F from="Mailgun Sandbox <postmaster@$MAILGUN_DOMAIN>" \
     -F to=$MAILGUN_TO \
-    -F subject="service is started!" \
-    -F text="started!"	
+    -F subject="$__DEFAULT_DOMAIN_NAME__ is started" \
+    -F text="$__DEFAULT_DOMAIN_NAME__ is started!"	
 fi
